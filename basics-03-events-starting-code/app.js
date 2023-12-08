974cfedb15dbbe8e19c5b28b9f89e6630af57f5b
Megan Smith <msmith@ag.com>
2023-12-08 12:12:25 -0500

    event args and modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,9 +1,31 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name: '',
+      confirmedName: ''
     };
   },
+  methods:{
+    add(num){
+      this.counter += num;
+    },
+    remove(num){
+      this.counter -= num;
+    },
+    // setName(event){
+    //   this.name = event.target.value;
+    // },
+    setName(event, lastName){
+      this.name = event.target.value + " " + lastName;
+    },
+    submitForm(){
+      alert("Submitted!");
+    },
+    confirmNameInput(){
+      this.confirmedName = this.name
+    }
+  }
 });
 
 app.mount('#events');
