49d75d2c2de467a3485e508a358e34ee06613e34
Megan Smith <msmith@ag.com>
2023-12-08 13:31:35 -0500

    2-way-binding, computed properties and watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,12 +2,51 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: ''
     };
   },
+  //best used when you run to some code which may also update some data property 
+  //in reaction to a property change i.e http request, timers
+  watch: {
+    counter(value){
+      if(value > 50 ){
+        this.counter = 0
+      }
+    }
+    // name(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }else{
+    //     this.fullname = value + "" + this.lastName;
+    //   }
+    // },
+
+    // lastName(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }else{
+    //     this.fullname = this.name + "" + value;
+    //   }
+    // }
+  },
+  //computed properties, better for performance for outputting values
+  //and when there's more than 1 dependacy
+  computed: { 
+    fullname(){
+        console.log("running again")
+        if(this.name === '' || this.lastName === ''){
+          return  '';
+        }
+  
+        return this.name + " " + this.lastName;
+    }
+  },
+  //methods are good to call when you want to re-calculate a value 
+  //whenever anything on the page changes or when an event occurs
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,6 +54,19 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+    resetInput(){
+      this.name = "";
+    },
+    //not good, because this method gets called whenever we call add() or reduce()
+    //vue calls ALL methods(whether we are calling them or not) by default
+    outputFullName(){
+      console.log("running again")
+      if(this.name === ''){
+        return  '';
+      }
+
+      return this.name + "" + "Smith";
     }
   }
 });
