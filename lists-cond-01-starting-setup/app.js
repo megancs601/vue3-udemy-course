bb7c71457ebe37ae04e040137ab7dc26186a0d7b
Megan Smith <msmith@ag.com>
2023-12-11 11:02:41 -0500

    v-if,v-else, v-for

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,7 +1,19 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      enteredGoal: '',
+      goals: [] 
+    };
   },
+  methods:{
+    addGoal(){
+      this.goals.push(this.enteredGoal);
+    },
+
+    removeGoal(index){
+      this.goals.splice(index, 1)
+    }
+  }
 });
 
 app.mount('#user-goals');
