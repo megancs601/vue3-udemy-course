4b49c59ad633addb8129e280b63fd4f85ea4693e
Megan Smith <msmith@ag.com>
2023-12-11 12:44:07 -0500

    battle log

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             monsterHp: 100,
             playerHp:100,
             currentRound:0,
-            winner: null
+            winner: null,
+            logMessages: []
         }
     },
     watch: {
@@ -41,22 +42,26 @@ const app = Vue.createApp({
         specialAttackAvailable(){
             return this.currentRound % 3 !==0
         }
+        
     },
     methods: {
         attackMonster(){
             this.currentRound++;
             const dmg = getRandomNum(5, 12);
             this.monsterHp -= dmg;
+            this.addLogMessage('player', 'attack', dmg);
             this.attackPlayer();
         },
         attackPlayer(){
             const dmg = getRandomNum(8, 15);
             this.playerHp -= dmg;
+            this.addLogMessage('monster', 'attack', dmg);
         },
         specialAttackMonster(){
             this.currentRound++;
             const dmg = getRandomNum(10,25);
             this.monsterHp -=dmg;
+            this.addLogMessage('player', 'attack', dmg);
             this.attackPlayer();
         },
         healPlayer(){
@@ -64,7 +69,7 @@ const app = Vue.createApp({
             const heal = getRandomNum(8,20);
             this.playerHp += heal;
             this.playerHp = this.playerHp > 100 ? 100:this.playerHp;
-
+            this.addLogMessage('player', 'heal', heal);
             this.attackPlayer();
         },
         startGame(){
@@ -72,9 +77,20 @@ const app = Vue.createApp({
             this.monsterHp = 100;
             this.winner = null;
             this.currentRound = 0;
+            this.logMessages = [];
         },
         surrender(){
             this.winner = "monster"
+        },
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            })
+        },
+        classNameFor(who){
+            return who === 'player' ? 'log--player': 'log--monster'
         }
     }
 });
