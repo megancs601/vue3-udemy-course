34b2188ad8e5a361a608db52ca12be0cef409c6e
Megan Smith <msmith@ag.com>
2023-12-19 14:46:14 -0500

    scoped slots

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,12 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2> {{ slotProps.item }}</h2>
+        <p> {{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,6 +20,7 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue'
 
 export default {
   //local components thats can only be used here, in this App.vue file
@@ -24,7 +31,8 @@ export default {
     //TheHeader: TheHeader
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo, 
+    CourseGoals
   },
   data() {
     return {
