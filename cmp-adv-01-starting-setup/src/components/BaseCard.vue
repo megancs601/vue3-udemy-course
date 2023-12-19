3ffd2d4a9c6b14b2f1864c6eaefb83a7a966a121
Megan Smith <msmith@ag.com>
2023-12-19 13:33:22 -0500

    introducing slots

---
+++ cmp-adv-01-starting-setup/src/components/BaseCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- slots allow us to insert html content while being able to use vue features-->
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+div {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+</style>
