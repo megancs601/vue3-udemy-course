07c3b0e24abc00c21312b063a2ad3f67c109c14e
Megan Smith <msmith@ag.com>
2023-12-20 14:40:24 -0500

    error dialog

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,6 +1,41 @@
 <template>
     <div>
         <h2> Manage Goals</h2>
-        <input type="text">
+        <input type="text" ref="goal">
+        <button @click="setGoal">Set Goal</button>
+        <error-alert v-if="inputIsInvalid">
+            <h2>Input is Invalid</h2>
+            <p>Please enter a few characters...</p>
+            <button @click="confirmError">Close</button>
+        </error-alert>
     </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+
+    export default {
+        components: {
+            ErrorAlert
+        },
+        data(){
+            return {
+                inputIsInvalid: false
+            };
+        },
+        methods: {
+            setGoal(){
+                const value = this.$refs.goal.value;
+
+                //     if(value ===''){
+                //         //alert('Input must not be empty!')
+                //     }
+                // }
+                this.inputIsInvalid = value === '';
+            },
+            confirmError(){
+                this.inputIsInvalid = false
+            }
+        }
+    }
+</script>
