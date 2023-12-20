2ec28b9e631e689d252a58c973222f4ecdd1b733
Megan Smith <msmith@ag.com>
2023-12-20 15:38:45 -0500

    first components and props

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <ul></ul>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.key"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+  components: {
+    LearningResource,
+  },
   data() {
     return {
       storedResources: [
