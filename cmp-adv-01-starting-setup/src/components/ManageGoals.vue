2ac48543fa4915208869855d8429ff757c17110f
Megan Smith <msmith@ag.com>
2023-12-20 14:42:36 -0500

    teleport element

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -3,11 +3,13 @@
         <h2> Manage Goals</h2>
         <input type="text" ref="goal">
         <button @click="setGoal">Set Goal</button>
-        <error-alert v-if="inputIsInvalid">
-            <h2>Input is Invalid</h2>
-            <p>Please enter a few characters...</p>
-            <button @click="confirmError">Close</button>
-        </error-alert>
+        <teleport to='body'>
+            <error-alert v-if="inputIsInvalid">
+                <h2>Input is Invalid</h2>
+                <p>Please enter a few characters...</p>
+                <button @click="confirmError">Close</button>
+            </error-alert>
+        </teleport>
     </div>
 </template>
 
