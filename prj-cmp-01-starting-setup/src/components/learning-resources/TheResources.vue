b10eb466bbd888ca22a433bfc4494e0ee82170b5
Megan Smith <msmith@ag.com>
2023-12-20 16:25:57 -0500

    adding and styling tabs

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,59 @@
+<template>
+  <base-card>
+    <!-- vue allows you to add events to custom components that fall through to the root element -->
+    <base-button @click="setTab('stored-resources')" :mode="storedResourceMode"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setTab('add-resource')" :mode="addResourceButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return { resources: this.storedResources };
+  },
+  computed: {
+    storedResourceMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResourceButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  methods: {
+    setTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
