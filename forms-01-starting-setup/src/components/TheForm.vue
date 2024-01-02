ffe909ef250155dc55cbfc1ced8801da9e0f6837
Megan Smith <msmith@ag.com>
2024-01-02 14:07:53 -0500

    v-model modifiers and numbers

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,12 +1,17 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+      />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model.number="userAge" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
@@ -51,6 +56,23 @@
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: null,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.userName = '';
+      this.userAge = null;
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
