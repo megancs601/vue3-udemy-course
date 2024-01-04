6db3388948811875b6c35c55bdf33c6901ddfd23
Megan Smith <msmith@ag.com>
2024-01-04 14:56:16 -0500

    route leave guards

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item
       v-for="user in users"
@@ -18,14 +19,32 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return { changesSaved: false };
+  },
   methods: {
     confirmInput() {
       this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.changesSaved = true;
     },
   },
   beforeRouteEnter(to, from, next) {
     console.log('UsersList beforeRouteEnter');
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList beforeRouteLeave');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm(
+        'Are you sure? You got unsaved changes!'
+      ); // returns t or f
+      next(userWantsToLeave);
+    }
   },
 };
 </script>
