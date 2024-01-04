1b31e9f33e52e72e2eb888a5a126648c475221e6
Megan Smith <msmith@ag.com>
2024-01-04 13:46:52 -0500

    redirecting  and catch all routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -9,15 +9,18 @@ import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' }, //changes url to localIp/teams
     {
       path: '/teams',
       component: TeamsList, // our domain.com/teams => TeamsList
+      //alias: '/' //works same way as { path: '/', redirect: '/teams' } but doesn't change the url
     },
     {
       path: '/users',
       component: UserList, // our domain.com/users => UsersList
     },
     { path: '/teams/:teamId', component: TeamMembers, props: true }, //teamId will be passed as a prop rather than stored in a $router.params
+    { path: '/:notFound(.*)', redirect: '/teams' }, // this comes last for lowest priority
   ],
 });
 
