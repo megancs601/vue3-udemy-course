b5babe48cc3483955a2d03aca9ee759e60e2a920
Megan Smith <msmith@ag.com>
2024-01-12 15:38:37 -0500

    adding coaches to vuex

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+  registerCoach(cxt, payload) {
+    const coachData = {
+      id: cxt.rootGetters.userId,
+      firstName: payload.first,
+      lastName: payload.last,
+      description: payload.desc,
+      hourlyRate: payload.rate,
+      areas: payload.areas,
+    };
+
+    cxt.commit('registerCoach', coachData);
+  },
+};
