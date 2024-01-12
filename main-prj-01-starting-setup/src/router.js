2e0a56cb63d6541ec32412b437d6c6ccb8d586ea
Megan Smith <msmith@ag.com>
2024-01-12 10:29:49 -0500

    wiring up pages

--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -15,7 +15,7 @@ const router = createRouter({
     { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
     {
-      path: '/coaches:id',
+      path: '/coaches/:id',
       component: CoachDetail,
       children: [{ path: 'contact', component: ContactCoach }],
     },
